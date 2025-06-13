d827f9b7075f5d2fa5f7241ab45c002a5c060d4d
Leeveo <101552154+leeveo@users.noreply.github.com>
2025-06-13 12:52:49 +0200

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,5 +5,9 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 export const supabaseServer = () => {
-  return createServerComponentClient({ cookies })
+  return createServerComponentClient({
+    cookies,
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  })
 }
